e9dcd05124f41d6cd6a5007c4a50d9b38817085b
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-05-21 14:55:01 +0300

    Update registration function to include password hashing

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import {Dao} from '../dao/Dao'
 import {Request, Response} from "express";
 import {IUser} from "../interfaces/IUser";
+import bcrpty from 'bcryptjs';
 
 export class UserController {
     private dao: Dao;
@@ -13,13 +14,14 @@ export class UserController {
     public registerCitizen(request: Request, response: Response) {
         const userDetails: IUser = request.body;
 
-        this.dao.addUser(userDetails).then((result) => {
-            response.status(200).json({
-                code: 200, message: result.message, userId: result.userId
-            });
-        }).catch((error) => {
-            response.status(400).json({
-                code: 400, message: 'Error registering user', debug: [{error: error}]
+        bcrpty.hash(userDetails.password, Number(process.env.SALT_ROUNDS)).then((hash) => {
+            userDetails.password = hash;
+
+            this.dao.addUser(userDetails).then((result) => {
+                response.status(200).json({code: 200, message: result.message, userId: result.userId});
+
+            }).catch((error) => {
+                response.status(400).json({code: 400, message: 'Error registering user', debug: [{error: error}]});
             });
         });
     }
