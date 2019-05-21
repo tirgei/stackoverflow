2541ed51255f1cc55d9942f6efb6566303336b33
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-05-21 11:10:49 +0300

    Add model interface for User object

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+    id: number,
+    firstName: string,
+    lastName: string,
+    surname: string,
+    phone: string,
+    email: string,
+    password: string,
+    createdAt: any,
+    modifiedAt: any
+}
