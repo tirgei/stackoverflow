e32ec969433c145fe3eaeb28fab9255972bfe529
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-05-23 10:42:29 +0300

    Fix middleware to validate auth token

--- src/middleware/token-validator.ts
+++ src/middleware/token-validator.ts
@@ -4,11 +4,11 @@ import jwt from 'jsonwebtoken';
 export const tokenMiddleware = (request: Request, response: Response, next: NextFunction) => {
     let token = request.header('authorization');
 
-    if (token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length);
-    }
-
     if (token) {
+        if (token.startsWith('Bearer ')) {
+            token = token.slice(7, token.length);
+        }
+
         jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
            if (error) {
                response.status(400).json({code: 400, message: 'Invalid token provided'})
