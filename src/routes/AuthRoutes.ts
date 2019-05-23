0653702b04a1779743a5914a6036d60df8f23b94
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-05-23 10:01:14 +0300

    Add class for authenticated routes and route for adding new question

---
+++ src/routes/AuthRoutes.ts
@@ -0,0 +1,26 @@
+import {Dao} from "../dao/Dao";
+import {Router} from "express";
+import {QuestionController} from "../controllers/QuestionController";
+import {validationMiddleware} from "../middleware/joi-validator";
+import {questionSchema} from "../joi/question-schema";
+
+export class AuthRoutes {
+    public router: Router;
+    private questionsController: QuestionController;
+
+    constructor(dao: Dao) {
+        this.router = Router();
+        this.questionRoutes();
+
+        this.questionsController = new QuestionController(dao);
+    }
+
+    private questionRoutes(): void {
+        // Add new question
+        this.router.post('/questions', validationMiddleware(questionSchema, 'body'), (req, res) => {
+           this.questionsController.newQuestion(req, res);
+        });
+
+    }
+
+}
