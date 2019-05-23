2b18182eee736b9a30439ff5d5267a3b3d77faf2
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-05-23 09:59:56 +0300

    Add function to validate access token for auth routes

---
+++ src/middleware/token-validator.ts
@@ -0,0 +1,24 @@
+import {NextFunction, Request, Response} from "express";
+import jwt from 'jsonwebtoken';
+
+export const tokenMiddleware = (request: Request, response: Response, next: NextFunction) => {
+    let token = request.header('authorization');
+
+    if (token.startsWith('Bearer ')) {
+        token = token.slice(7, token.length);
+    }
+
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+           if (error) {
+               response.status(400).json({code: 400, message: 'Invalid token provided'})
+           } else {
+               request.app.locals.userData = decoded;
+               next();
+           }
+        });
+
+    } else {
+        response.status(400).json({code: 400, message: 'No auth token provided'});
+    }
+};
