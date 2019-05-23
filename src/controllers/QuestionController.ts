38f57b78ec0a12b45cb78e118eeef2d44d90c9a5
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-05-23 10:02:35 +0300

    Create questions controller and add function to save new question

---
+++ src/controllers/QuestionController.ts
@@ -0,0 +1,24 @@
+import {Dao} from "../dao/Dao";
+import {Request, Response} from "express";
+import {IQuestion} from "../interfaces/IQuestion";
+
+export class QuestionController {
+    private dao: Dao;
+
+    constructor(dao: Dao) {
+        this.dao = dao;
+    }
+
+    // Function to add new question
+    public newQuestion(request: Request, response: Response) {
+        const question: IQuestion = request.body;
+        question.userId = request.app.locals.userData.id;
+
+        this.dao.addQuestion(question).then((result) => {
+           response.status(200).json({code: 200, message: result.message});
+        }).catch((error) => {
+            response.status(400).json({code: 400, message: 'Error adding question', debug: error})
+        });
+    }
+
+}
