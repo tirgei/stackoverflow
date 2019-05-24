96e9189e5e92b2994efcc7597442f7f1886d52b4
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-05-24 10:54:29 +0300

    Add db sql query for question tags table

--- src/utils/database-tables.ts
+++ src/utils/database-tables.ts
@@ -1,29 +1,39 @@
 // List of tables
 export const tables = [
     'users',
-    'questions'
+    'questions',
+    'question_tags'
 ];
 
 // Queries to create database tables
 export const createTableQueries = [
     "CREATE TABLE IF NOT EXISTS users (" +
-    "        id SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT," +
+    "        id INT NOT NULL AUTO_INCREMENT," +
     "        firstName VARCHAR(250) NOT NULL," +
     "        lastName VARCHAR(250) NOT NULL," +
     "        surname VARCHAR(250) NULL," +
-    "        username VARCHAR(250) NOT NULL," +
+    "        username VARCHAR(250) NOT NULL UNIQUE," +
     "        phone VARCHAR(250) NOT NULL," +
-    "        email VARCHAR(250) NOT NULL," +
+    "        email VARCHAR(250) NOT NULL UNIQUE," +
     "        password VARCHAR(250) NOT NULL," +
     "        createdAt VARCHAR(250) NOT NULL," +
-    "        modifiedAt VARCHAR(250) NOT NULL ) ENGINE=InnoDB",
+    "        modifiedAt VARCHAR(250) NOT NULL," +
+    "        PRIMARY KEY (id))",
 
     "CREATE TABLE IF NOT EXISTS questions (" +
-    "        id SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT," +
+    "        id INT NOT NULL AUTO_INCREMENT," +
     "        title VARCHAR(250) NOT NULL," +
     "        description VARCHAR(1000) NOT NULL," +
-    "        userId INTEGER(250) NULL," +
-    "        tags VARCHAR(250) NULL," +
+    "        userId INT NOT NULL," +
     "        createdAt VARCHAR(250) NOT NULL," +
-    "        modifiedAt VARCHAR(250) NOT NULL )" ,
+    "        modifiedAt VARCHAR(250) NOT NULL," +
+    "        PRIMARY KEY (id)," +
+    "        FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE)",
+
+    "CREATE TABLE IF NOT EXISTS question_tags (" +
+    "        id INT NOT NULL AUTO_INCREMENT," +
+    "        questionId INT(250) NOT NULL," +
+    "        tag VARCHAR(250) NOT NULL," +
+    "        PRIMARY KEY (id)," +
+    "        FOREIGN KEY (questionId) REFERENCES questions(id) ON DELETE CASCADE)"
 ];
